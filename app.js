854be457ccc59fe611c4bfe285baec04ae199fc3
audreyfly <95317631+audreyfly@users.noreply.github.com>
2024-01-04 14:49:02 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
+var readlineSync = require('readline-sync')
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/sheep.proto"
-var packageDefinition = protoLoader.loadSync(
-  PROTO_PATH
-)
+var PROTO_PATH = __dirname + "/protos/grassactivationService.proto"
 
-var movies_proto = grpc.loadPackageDefinition(packageDefinition).sheep
+var packageDefinition = protoLoader.loadSync(PROTO_PATH)
+var proto = grpc.loadPackageDefinition(packageDefinition).grassactivationService
+var client = new grassactivationservice_proto.grassactivationService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
-var data = [
-  {
-    sheepType:"adult",
+var call=client.grassactivationService()
+call.on('data',function (resp){
+console.log(resp.name has made( [data][username,password,locatiion,drainage] )
+  })
 
-  },
-  {
-    SheepNumber:"",
+call.on("end",function(){
+  })
 
-  },
-  {
-    sheepLocation:"dublin",
+  call.on("error",function(e){
+    console.log("an error occurred")
+  })
 
+  varuser=readlineSync.question("where is the location?")
 
+  console.log("where is the location?(q to quit)")
+  var readline = require('readline'),
+  rl = readline.createInterface(process.stdin, process.stdout);
 
-function getData(call,callback){
-for (var i=0;i<data.length;i++){
-call.write({
-sheepType:data[i].sheepType,
-})
-}
-call.end()
-
-function getData(call,callback){
-for (var i=0;i<data.length;i++){
-call.write({
-sheepNumber:data[i].SheepNumber,
-})
-}
-call.end()
-
-function getData(call,callback){
-for (var i=0;i<data.length;i++){
-call.write({
-sheepLocation:data[i].SheepLocation,
-})
-}
-call.end()
-
-
-var server = new grpc.Server()
-server.addService(sheep_proto.sheepService.service,{getData;getData})
-server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
-  server.start()
-})
-}
+    rl.on('line',function (line){
+       switch(line.trim()) {
+      case 'hello':
+        console.log('world!');
+        break;
+      default:
+        console.log('What is your userName `' + line.trim() + '`');
+        break;
+    }
+    rl.setPrompt(prefix, prefix.length);
+    rl.prompt();
+  }).on('close', function() {
+    console.log('Please enter your password ');
+    process.exit(0);
+  });
+  console.log(prefix + 'What service do you require .');
+  rl.setPrompt(prefix, prefix.length);
+  rl.prompt();
