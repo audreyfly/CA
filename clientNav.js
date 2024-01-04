9a06395b812055b36bec817b069d54707144295b
audreyfly <95317631+audreyfly@users.noreply.github.com>
2024-01-04 15:48:30 +0000

    Add files via upload

---
+++ clientNav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Nav=()=>{
+return(
+  <div>classname="Nav-ul">
+<ul>
+<Li><Link to= ""/"">type</Link</Li>
+<Li><Link to= ""/"">Location</Link</Li>
+<Li><Link to= ""/"">username</Link</Li>
+<Li><Link to= ""/"">password</Link</Li>
+<Li><Link to= ""/"">tagNumber</Link</Li>
+</ul>
+</div>
+)
+}
+export default Nav;
